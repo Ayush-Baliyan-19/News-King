490a2d9c641a5ca0292be7cc26b9458b39e20d1b
ayush-baliyan-19 <ayush.3316403221@ipu.ac.in>
2022-07-14 12:57:42 +0530

    Testing on new lappy

--- src/Component/Loading/Loading.js
+++ src/Component/Loading/Loading.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react'
 export default class Loading extends Component {
   render() {
     return (
-      <div>hello this is loading page</div>
+      <div>Hahahahaha</div>
     )
   }
 }
